7ccef49ca892fc2e49a0b6f9e87040bba543513e
squaredx <jason.wolfe@memorykpr.com>
2023-09-27 15:03:19 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/aar_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aar_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,291 @@
+import 'dart:async';
+import 'dart:io' as io;
+
+import 'package:another_audio_recorder/another_audio_recorder.dart';
+import 'package:file/file.dart';
+import 'package:flutter/material.dart';
+import 'package:file/local.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage(
+      {super.key,
+      required this.title,
+      this.localFileSystem = const LocalFileSystem()});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+  final LocalFileSystem localFileSystem;
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  AnotherAudioRecorder? _recorder;
+  Recording? _current;
+  RecordingStatus _currentStatus = RecordingStatus.Unset;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print(_currentStatus);
+                          switch (_currentStatus) {
+                            case RecordingStatus.Initialized:
+                              {
+                                _start();
+                                break;
+                              }
+                            case RecordingStatus.Recording:
+                              {
+                                _pause();
+                                break;
+                              }
+                            case RecordingStatus.Paused:
+                              {
+                                _resume();
+                                break;
+                              }
+                            case RecordingStatus.Stopped:
+                              {
+                                _init();
+                                break;
+                              }
+                            default:
+                              break;
+                          }
+                        },
+                        child: _buildText(_currentStatus),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: _currentStatus != RecordingStatus.Unset
+                          ? _stop
+                          : null,
+                      child: Text("Stop"),
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    ElevatedButton(
+                      onPressed: onPlayAudio,
+                      child: Text(
+                        "Play",
+                      ),
+                    ),
+                  ],
+                ),
+                Text("Status : $_currentStatus"),
+                Text('Avg Power: ${_current?.metering?.averagePower}'),
+                Text('Peak Power: ${_current?.metering?.peakPower}'),
+                Text("File path of the record: ${_current?.path}"),
+                Text("Format: ${_current?.audioFormat}"),
+                Text(
+                    "isMeteringEnabled: ${_current?.metering?.isMeteringEnabled}"),
+                Text("Extension : ${_current?.extension}"),
+                Text(
+                    "Audio recording duration : ${_current?.duration.toString()}")
+              ]),
+        ),
+      ),
+    );
+  }
+
+  _init() async {
+    try {
+      await Permission.microphone.request();
+      //if (await AnotherAudioRecorder.hasPermissions) {
+      String customPath = '/another_audio_recorder_';
+      io.Directory appDocDirectory;
+//        io.Directory appDocDirectory = await getApplicationDocumentsDirectory();
+      if (io.Platform.isIOS) {
+        appDocDirectory = await getApplicationDocumentsDirectory();
+      } else {
+        appDocDirectory = (await getExternalStorageDirectory())!;
+      }
+
+      // can add extension like ".mp4" ".wav" ".m4a" ".aac"
+      customPath = appDocDirectory.path +
+          customPath +
+          DateTime.now().millisecondsSinceEpoch.toString();
+
+      // .wav <---> AudioFormat.WAV
+      // .mp4 .m4a .aac <---> AudioFormat.AAC
+      // AudioFormat is optional, if given value, will overwrite path extension when there is conflicts.
+      _recorder =
+          AnotherAudioRecorder(customPath, audioFormat: AudioFormat.AAC);
+
+      await _recorder?.initialized;
+      // after initialization
+      var current = await _recorder?.current(channel: 0);
+      print(current);
+      // should be "Initialized", if all working fine
+      setState(() {
+        _current = current;
+        _currentStatus = current!.status!;
+        print(_currentStatus);
+      });
+      // } else {
+      //   await Permission.microphone.request();
+
+      //   // Scaffold.of(context).showSnackBar(
+      //   //     new SnackBar(content: new Text("You must accept permissions")));
+      // }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _start() async {
+    try {
+      await _recorder?.start();
+      var recording = await _recorder?.current(channel: 0);
+      setState(() {
+        _current = recording;
+      });
+
+      const tick = const Duration(milliseconds: 50);
+      Timer.periodic(tick, (Timer t) async {
+        if (_currentStatus == RecordingStatus.Stopped) {
+          t.cancel();
+        }
+
+        var current = await _recorder?.current(channel: 0);
+        // print(current.status);
+        setState(() {
+          _current = current;
+          _currentStatus = _current!.status!;
+        });
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _resume() async {
+    await _recorder?.resume();
+    setState(() {});
+  }
+
+  _pause() async {
+    await _recorder?.pause();
+    setState(() {});
+  }
+
+  _stop() async {
+    var result = await _recorder?.stop();
+    print("Stop recording: ${result?.path}");
+    print("Stop recording: ${result?.duration}");
+    File file = widget.localFileSystem.file(result?.path);
+    print("File length: ${await file.length()}");
+    setState(() {
+      _current = result;
+      _currentStatus = _current!.status!;
+    });
+  }
+
+  Widget _buildText(RecordingStatus status) {
+    var text = "";
+    print(status);
+    switch (_currentStatus) {
+      case RecordingStatus.Initialized:
+        {
+          text = 'Start';
+          break;
+        }
+      case RecordingStatus.Recording:
+        {
+          text = 'Pause';
+          break;
+        }
+      case RecordingStatus.Paused:
+        {
+          text = 'Resume';
+          break;
+        }
+      case RecordingStatus.Stopped:
+        {
+          text = 'Init';
+          break;
+        }
+      default:
+        break;
+    }
+    return Text(text, style: TextStyle(color: Colors.white));
+  }
+
+  void onPlayAudio() async {
+    AudioPlayer audioPlayer = AudioPlayer();
+    //await audioPlayer.play(_current?.path ?? '');
+  }
+}
